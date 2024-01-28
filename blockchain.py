6c3a36c07f3f8832250a76e5550ee583848f5f26
wallacevncs <wallacevncs@gmail.com>
2024-01-28 20:45:42 -0300

    Adding the connect_node and update_chain endpoints. Also, adding consensus protocol

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,21 @@
 import os
+import shutil
 import re
 import datetime
 import hashlib
 import json
+import requests
+from collections import OrderedDict
 from urllib.parse import urlparse
 from matching.games import HospitalResident
 
 class Blockchain:
 
-    def __init__(self):
-        self.chain = []
-        self.nodes = set()
+    def __init__(self, path, trash_path):
+        self.path        = path
+        self.trash_path  = trash_path
+        self.chain       = []
+        self.nodes       = set()
 
     def create_block(self, poUW, previous_hash, edition):
         block = {'index': len(self.chain) + 1,
@@ -26,6 +31,84 @@ class Blockchain:
         if not self.chain:
             return None
         return self.chain[-1]
+
+    def is_chain_valid(self, chain, block):
+
+        last_block = self.get_previous_block()
+        for i in range(len(chain) - 1, -1, -1):
+            current_block = chain[i]
+            year_match    = re.compile(r'\d{4}').search(current_block['edition'])
+            if not year_match:
+                continue
+
+            edition_year  = year_match.group(0)
+
+            # Compares the last result found between the 2 chains
+            if current_block['edition'] == last_block['edition'] and current_block['result'] != last_block['result']:
+                
+                residentsPreferencesFilePath  = '{}/residentsPreferences_{}.json'.format(self.trash_path, edition_year)
+                hospitalsPreferencesFilePath  = '{}/hospitalsPreferences_{}.json'.format(self.trash_path, edition_year)
+                
+                # Returns files to be reprocessed
+                self.move_files(residentsPreferencesFilePath, self.path)
+                self.move_files(hospitalsPreferencesFilePath, self.path)
+
+                # Remove the last block
+                self.chain.pop()
+
+                return False
+            
+            files = self.list_files(self.trash_path, edition_year)
+            if not files and i == len(chain) - 1:
+                self.chain.pop()
+                return False
+
+            if i == 0:
+                continue
+
+            previous_block = chain[i-1]
+            if current_block['previous_hash'] != self.hash(previous_block):
+                return False
+            
+        return True
+
+    def update_chain(self):
+        network             = self.nodes
+        updated_chain       = None
+        max_length          = len(self.chain)
+        current_timestamp   = None
+
+        if self.chain and 'timestamp' in self.chain[-1]:
+            current_timestamp = datetime.datetime.strptime(self.chain[-1]['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+
+        for node in network:
+            response   = requests.get(f'http://{node}/get_chain')
+
+            if response.status_code == 200:
+                length         = response.json()['length']
+                chain          = response.json()['chain']
+
+                if(len(chain) == 0):
+                    continue
+
+                node_timestamp = None
+                if chain and 'timestamp' in chain[-1]:
+                    node_timestamp = datetime.datetime.strptime(chain[-1]['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+
+                if not self.is_chain_valid(chain, chain[-1]):
+                    continue
+
+                if length > max_length:
+                    max_length    = length
+                    updated_chain = chain
+                elif length == max_length and current_timestamp > node_timestamp:
+                    updated_chain = chain
+
+        if updated_chain:
+            self.chain = updated_chain
+            return True
+        
+        return False
 
     def proof_of_work(self, residentsPreferencesFilePath, hospitalsPreferencesFilePath):
 
@@ -69,8 +152,8 @@ class Blockchain:
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
-    def list_files(self, folder_path):
-        files_by_year = {}
+    def list_files(self, folder_path, year=None):
+        files_by_year = OrderedDict()
 
         # List all files in the folder
         files = os.listdir(folder_path)
@@ -82,13 +165,26 @@ class Blockchain:
             result = year_pattern.search(file)
 
             if result:
-                year = result.group(0)
+                file_year = result.group(0)
+
+                if year and file_year != year:
+                    continue
+
                 full_path = os.path.join(folder_path, file)
 
-                if year not in files_by_year:
-                    files_by_year[year] = []
+                if file_year not in files_by_year:
+                    files_by_year[file_year] = []
 
                 # Add to the dictionary with the year as the key and the path as the value
-                files_by_year[year].append(full_path)
+                files_by_year[file_year].append(full_path)
+
+        files_by_year = OrderedDict(sorted(files_by_year.items(), key=lambda x: x[0], reverse=True))
 
         return files_by_year
+    
+    def move_files(self, current_path, subdirectory_path):
+        os.makedirs(subdirectory_path, exist_ok=True)
+
+        if os.path.exists(current_path):
+            destination_path = os.path.join(subdirectory_path, os.path.basename(current_path))
+            shutil.move(current_path, destination_path)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import shutil
+import os
 import sys
 from flask import Flask, jsonify, request
 import requests
@@ -12,13 +14,16 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 node_address = str(uuid4()).replace('-', '')
 
-blockchain = Blockchain()
+path        = ''
+trash_path  = ''
 
-path = "/mnt/c/Users/walla/Documents/Faculdade/Projeto Final/Files/INPUT"
-
+blockchain  = Blockchain(path, trash_path )
 
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
+
+    blockchain.update_chain()
+
     files = blockchain.list_files(path)
     if not files:
         return jsonify({'message': 'There are no files to be mined.'}), 204
@@ -40,14 +45,41 @@ def mine_block():
                 'previous_hash': block['previous_hash'],
                 'result':        block['result']}
     
+    # Remove files from the processing queue
+    blockchain.move_files(residentsPreferencesFilePath, trash_path)
+    blockchain.move_files(hospitalsPreferencesFilePath, trash_path)
+
     return jsonify(response), 200
 
 
 @app.route('/get_chain', methods=['GET'])
 def get_chain():
+
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "Vazio", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message':  'Connected node. Blockchain contains the following connected nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    
+    return jsonify(response),201
+
+@app.route('/update_chain',methods= ['GET'])
+def update_chain():
+
+    if blockchain.update_chain():
+        response = {'message': 'Blockchain successfully updated.'}
+    else:
+        response = {'message': 'Current blockchain version is already updated.'}
+
+    return jsonify(response), 200
 
 app.run(host='0.0.0.0', port=5001)
